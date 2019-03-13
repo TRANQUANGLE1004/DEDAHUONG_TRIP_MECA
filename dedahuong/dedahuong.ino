a3d7677ff3b155be86bb45cd0f5691bd8ddd6cbc
quangletran <quangletran@hotmail.com>
2019-03-14 00:06:00 +0700

    hihi

--- dedahuong/dedahuong.ino
+++ dedahuong/dedahuong.ino
@@ -25,13 +25,16 @@ void setup() {
 	pinMode(PIN_PULSE_MECANUM_RIGHT, OUTPUT);
 	pinMode(PIN_DIR_MECANUM_RIGHT, OUTPUT);
 	//freeRun();
-	cli();
-	pulseGenMecanum(1000,1000);
+	sei();
+	//
+	//fucking(1, 100);
+	hamkhoidong();
 }
 
 void loop() {
 
 }
+
 
 // the loop function runs over and over again until power down or reset
 //void loop() {
@@ -44,22 +47,30 @@ void loop() {
 //	{
 //	case 'F':
 //		//Forward
-//		runStraight(1000,800);
+//		runStraight(5000,4000);
+//		//hamkhoidong();
 //		Serial.println("run run straight");
 //		break;
 //	case 'B':
-//		runStraight(-10000, -8000);
+//		runStraight(-5000, -4000);
 //		Serial.println("run run back");
 //		break;
 //	case 'L':
 //		// turn left
-//		runStraight(-10000, 8000);
+//		runStraight(-5000, 4000);
 //		//runLeft(speed);
 //		break;
 //	case'R':
-//		runStraight(10000, -8000);
+//		runStraight(5000, -4000);
 //		//turn right
 //		//runRight(speed);
+//		break;
+//
+//	case'J':
+//		rotateClockWise(5);
+//		break;
+//	case'G':
+//		rotateInvertClockWise(5);
 //		break;
 //	case'S':
 //		//free
@@ -71,17 +82,68 @@ void loop() {
 //}
 
 ISR(TIMER3_OVF_vect) {
-	//TCCR3B &= 0xF8; //stop timer 3
-	TCCR1B &= 0xF8; //stop timer 1
-	//OCR1A = pwmValA;
-	TIMSK3 &= 0x00;
-	TCCR3B &= 0xF8;
-	Serial.println("ISR OVF vect 3");
+	if (count == 0) {
+		TCCR3B |= (1 << CS32);
+		TCNT3 = timer3;
+		count++;
+		Serial.println("count == 0");
+	}
+	else if (count==1) {
+		/*TCCR3B &= 0xF8;
+		TCCR1B &= 0xF8;
+		count = 0;
+		Serial.println("count != 0");*/
+		TCNT3 = timer3;
+		OCR1A = fre / 4;
+		OCR1C = fre / 4;
+		ICR1 = fre/2;
+		count++;
+		//Serial.println("count == 1");
+	}
+	else if (count == 2) {
+		//TCCR3B &= 0xF8;
+		TCNT3 = timer3;
+		OCR1A = fre / 8;
+		OCR1C = fre / 8;
+		ICR1 = fre / 4;
+		count++;
+		//TCCR3B |= (1<<CS32);
+	}
+	else if (count == 3) {
+		//TCCR3B &= 0xF8;
+		TCNT3 = timer3;
+		OCR1A = fre / 16;
+		OCR1C = fre / 16;
+		ICR1 = fre / 8;
+		count++;
+
+		//TCCR3B |= (1 << CS32);
+	}
+	else if (count == 4) {
+		//TCCR3B &= 0xF8;
+		TCNT3 = timer3;
+		OCR1A = fre / 32;
+		OCR1C = fre / 32;
+		ICR1 = fre / 16;
+		count++;
+
+		//TCCR3B |= (1 << CS32);
+	}
+	else if (count == 5) {
+		//TCCR3B &= 0xF8;
+		TCNT3 = timer3;
+		OCR1A = fre / 64;
+		OCR1C = fre / 64;
+		ICR1 = fre / 32;
+		count++;
+		//TCCR3B |= (1 << CS32);
+		TCCR1B &= 0xF8;
+	}
 }
 ISR(TIMER5_OVF_vect) {
 	TCCR5B &= 0xF8; //stop timer
 	TCCR4B &= 0xF8;
 	//OCR4A = pwmValB;
 	//TCNT5 = timeValB;
-	//Serial.println("hihi");
+	Serial.println("hihi 5");
 }
--- dedahuong/pwmTimer.h
+++ dedahuong/pwmTimer.h
@@ -4,14 +4,30 @@
 #include<avr/interrupt.h>
 #include<Arduino.h>
 #define fd	(1.0f/16000000.0f)	// frequency default atmega 2560
+#define fre		16000
 /*
 use pin ???
 */
+
+volatile int count = 0;
+unsigned short timer3;
 void genPWMTimer1(int frequency);
 void genPWMTimer4(int frequency);
-void settingPulseUseTimer3(int _time);
+unsigned short settingPulseUseTimer3(int _time);
 void settingPulseUseTimer5(int _time);
 // _num is pulse/1s ex: 1000 pulse/second
+
+//void controlOmni(int _num) {
+//	if (_num > 0) {
+//		digitalWrite(PIN_DIR_OMNI, DIRECTION_OMNI);
+//		genPWMTimer4(_num);
+//	}
+//	else {
+//		digitalWrite(PIN_DIR_OMNI, !DIRECTION_OMNI);
+//		genPWMTimer4(_num);
+//	}
+//}
+
 void pulseGenMecanum(int _num, int _time = 0) { //use timer 1 & timer 3
 	if (_time == 0) {
 		if (_num > 0) {
@@ -32,8 +48,8 @@ void pulseGenMecanum(int _num, int _time = 0) { //use timer 1 & timer 3
 			digitalWrite(PIN_DIR_MECANUM_LEFT, DIRECTION_MECANUM_LEFT);
 			digitalWrite(PIN_DIR_MECANUM_RIGHT, DIRECTION_MECANUM_RIGHT);
 			genPWMTimer1(_num);
-			//Serial.println("haha");
-			settingPulseUseTimer3(_time);
+			delay(1000);
+			Serial.println("haha1");
 			//TCCR3B |= (1 << CS32);// bat timer3 || prescaler = 256
 		}
 		else {
@@ -88,12 +104,12 @@ void genPWMTimer1(int frequency) {//use timer1
 	TCCR1B |= (1 << CS10); // khoi dong pwm
 }
 
-void settingPulseUseTimer3(int _time) {
+unsigned short settingPulseUseTimer3(int _time) {
 	// time unit is ms
 	//setting timer 3
 	//reset reg
-	TCCR3A = 0;
 	TCCR3B = 0;
+	TCCR3A = 0;
 	TIMSK3 = 0;
 	//int prescaler;
 	/*	table choose prescaler		CSn2	CSn1	CSn0
@@ -111,10 +127,11 @@ void settingPulseUseTimer3(int _time) {
 	float t = (float)_time / (256.0f*(1.0f / 16000.0f));
 	// I use Nomal Mode 
 	unsigned short _val = 65535 - (unsigned short)t - 1;
-	TCNT3 = _val;
-	TIMSK3 |= (1 << TOIE3); //cho phep ngat tran
-	TCCR3B |= (1 << CS32);
 	//Serial.println(_val);
+	TCNT3 = _val;
+	//TCCR3B |= (1 << CS32);
+	TIMSK3 |= (1 << TOIE3); //cho phep ngat tran
+	return _val;
 }//use timer 3
 
 void settingPulseUseTimer5(int _time) {
@@ -170,6 +187,42 @@ void runStraight(int _numMeca, int _numOm) {
 	pulseGenMecanum(_numMeca);
 	pulseGenOmni(_numOm);
 }
+void rotateClockWise(int _num) {
+	digitalWrite(PIN_DIR_MECANUM_LEFT, DIRECTION_MECANUM_LEFT);
+	digitalWrite(PIN_DIR_MECANUM_RIGHT, !DIRECTION_MECANUM_RIGHT);
+	digitalWrite(PIN_DIR_OMNI, !DIRECTION_OMNI);
+	genPWMTimer4((int)((float)_num*800.0f*1.55f));
+	genPWMTimer1(_num*1000);
+}
 
+void rotateInvertClockWise(int _num) {
+	digitalWrite(PIN_DIR_MECANUM_LEFT, !DIRECTION_MECANUM_LEFT);
+	digitalWrite(PIN_DIR_MECANUM_RIGHT, DIRECTION_MECANUM_RIGHT);
+	digitalWrite(PIN_DIR_OMNI, DIRECTION_OMNI);
+	genPWMTimer4((int)((float)_num*800.0f*1.55f));
+	genPWMTimer1(_num * 1000);
+}
+
+
+
+void fucking(int _numRota, int _time) {
+	genPWMTimer1(_numRota*1000);////// mecanum
+	genPWMTimer4(_numRota*800);
+	timer3 = settingPulseUseTimer3(_time);
+}
+
+void increasePulse(int _numPulse = 10000, int _time = 1000) {
+	fucking(1, 50);
+}
+void hamkhoidong(){
+	//digitalWrite(PIN_DIR_MECANUM_LEFT, DIRECTION_MECANUM_LEFT);
+	//digitalWrite(PIN_DIR_MECANUM_RIGHT, DIRECTION_MECANUM_RIGHT);
+	//digitalWrite(PIN_DIR_OMNI, DIRECTION_OMNI);
+	ICR1 = fre;
+	OCR1A = fre/2;
+	OCR1C = fre/2;
+	TCCR1B |= (1 << CS10); // khoi dong pwm
+	timer3 = settingPulseUseTimer3(50);
+}
 
 #endif // !_PWM_TIMER_H_
